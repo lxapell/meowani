b88ae04c5e0c8cf3f5d0150513f66de21ff8367e
Mocha1530 <astherielle360@gmail.com>
2026-05-01 19:17:43 +0000

    Adding metadata routes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,8 @@ export default function robots(): MetadataRoute.Robots {
       allow: [
         "/$",
         "/assets/*",
-        "/_next/*",
+        "/~/image",
+        "/_next/static/*",
         "/library",
         "/library/anime/*",
         "/library/watch/*",
